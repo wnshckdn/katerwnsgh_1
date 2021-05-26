fe9fc070ff2e8a686e251c2135dbc20e5272acda
Project1 <kidykjh@gmail.com>
2021-05-26 11:50:40 +0900

    add Bulma CSS

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
+    <nav class="breadcrumb">
+        <ul>
+          <li><nuxt-link to><i class="fas fa-home"></i>홈으로</nuxt-link></li>
+        </ul>
+    </nav>
     <nuxt/>
+    <footer class="footer">
+  <div class="content has-text-centered">
+    <i class="far fa-copyright">직장인에게 필요한 알람 여기에 있습니다.</i>
+    
+  </div>
+</footer>
   </div>
 </template>
 
