087fb9d32570ac39240513a3043d35212e7304e8
Project1 <kidykjh@gmail.com>
2021-06-02 09:51:21 +0900

    add tashu message and homepage tables

--- Upload.bat
+++ Upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "add tables for bus"
+git commit -m "add tashu message and homepage tables"
 git push -u origin main
--- pages/index.vue
+++ pages/index.vue
@@ -12,18 +12,25 @@
     </section>
     <br>
     <div class="columns">
-      <div class="column">
+      <div class="column1">
           <article class="message">
             <div class="message-header">
               <p>알람, 하루의 상쾌함</p>
             </div>
             <div class="message-body">
-              <figure class="image is-128x128">
+              <div class="columns">
+                <div class="column1">
+                  <figure class="image is-128x128">
                 <img src="https://previews.123rf.com/images/tmricons/tmricons1510/tmricons151000008/45799794-%EC%95%8C%EB%9E%8C-%EC%95%84%EC%9D%B4%EC%BD%98-.jpg">
                 </figure>
-              <figure class="image is-128x128">
-                <img src="~assets/alarm.png" alt="">
-              </figure>
+                </div>
+                <div class="column2">
+                  <figure class="image is-128x128">
+                  <img src="~assets/alarm.png" alt=""> 
+                </figure>
+                </div>
+              </div>
+              <br>
                 Alarm은 불안, 공포 및 경보(음), 그리고 자명종을 의미하는 영단어이다.
 한국에서는 '모닝콜'이라는 말을 자주 쓰고 있는데 이 단어의 어원은 영어가 아니라 재플리시인 モーニングコール이다. 참고로 모닝콜은 호텔 등에서 아침에 고객을 전화로 깨워준다는 것뿐 아니라, 아침에 타인이 전화를 걸어서 깨워준다는 의미도 포함한다. 재플리시라서 당연히 외국에선 통하지 않는다. 특히 영어권에서 morning call이라는 이야기를 아침에 호텔에서 하면 아침 회의로 착각할 수 있다. 전화로 깨워 주는 서비스를 원한다면 '웨이크 업 콜(wake-up call)'이라고 해야 한다.
             </div>
@@ -52,6 +59,61 @@
           <tr>
             <td>4</td>
             <td>6:25</td>
+          </tr>
+        </tbody>
+      </table>
+      </article>
+      </div>
+      
+      <div class="column2">
+        
+          <article class="message">
+            <div class="message-header">
+              <p>타슈, 활기찬 아침을 책임진다</p>
+            </div>
+            <div class="message-body">
+              <div class="columns">
+                <div class="column1">
+                  <figure class="image is-128x128">
+                
+                <img src="http://m.tashu.or.kr/htmls/mobile/images/visual-text.png">
+                </figure>
+                </div>
+                <div class="column2">
+                  <figure class="image is-128x128">
+                <img src="~assets/tashu.jpg" alt="">
+                </figure>
+                </div>
+              </div>
+              <br>
+               타슈는 2008년 부터 대전광역시에서 운영되고 있는 무인 대여 공공자전거 서비스다. 둔산신도시 및 도안신도시를 중심으로 주변지역에 집중적으로 비치되어 있으며, 주요 관공서나 지하철역 근처에도 비치되어 있다. 현재 중구와 대덕구, 동구의 대여소가 크게 확충되어 대부분의 대전광역시 전역으로 타슈가 확장되어 음영지역을 제외하고 대전 도심의 대부분에서 타슈를 이용할 수 있다. 또한 정부세종청사에도 대여소가 하나 있었지만 현재는 어울링 대여소로 변경되어서 운영 중에 있다.
+              </div>
+      </article>
+      
+      <article class="notification">
+      <table class="table is-striped is-hovable is-fullwidth">
+        <thead>
+            <tr>
+              <th>자전거 대여</th>
+              <th>타슈 페이지</th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>이용방법</td>
+            <td><a href="https://www.tashu.or.kr/introduceAction.do?process=howtorent">페이지 이동</a></td>
+          </tr>
+          <tr>
+            <td>실시간 현황</td>
+            <td><a href="https://www.tashu.or.kr/status.do?process=userStatusView&left=2">페이지 이동</a></td>
+          </tr>
+          <tr>
+            <td>대여소 정보</td>
+            <td><a href="https://www.tashu.or.kr/userStationAction.do?process=stationTotalList">페이지 이동</a></td>
+          </tr>
+          <tr>
+            <td>의무와 책임</td>
+            <td><a href="https://www.tashu.or.kr/introduceAction.do?process=charges&left=1">페이지 이동</a></td>
           </tr>
         </tbody>
       </table>
