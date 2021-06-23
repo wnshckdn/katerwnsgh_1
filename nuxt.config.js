bf3abdb9aa7126ceb263ed242fa8cb0eed313a6c
Project1 <kidykjh@gmail.com>
2021-06-23 21:58:16 +0900

    edit worldBicycle

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
      ** Headers of the page
      */
     head: {
-        title: '상쾌한 아침',
+        title: '내 알람',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
