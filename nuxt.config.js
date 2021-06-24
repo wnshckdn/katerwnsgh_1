fe05a55da04301de2202af02692e9fe4168c6c23
Project1 <kidykjh@gmail.com>
2021-06-24 15:34:34 +0900

    edit every pages and end

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
      ** Headers of the page
      */
     head: {
-        title: '내 알람',
+        title: '활기찬 아침',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
